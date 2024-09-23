d54bc2d8e4c31a82d097ec8231b24ff791729298
Hiperiosity <anastasy_ya@mail.ru>
2024-09-23 08:43:32 +0300

    gh-pages

--- src/components/Constants.js
+++ src/components/Constants.js
@@ -1,4 +1,4 @@
-export const API = "http://localhost:5000";
+export const API = "http://localhost:5000"; //http://localhost:5000"; "https://simple-scene-api.vercel.app"
 
 // Индексы вершин для граней
 export const indices = [
